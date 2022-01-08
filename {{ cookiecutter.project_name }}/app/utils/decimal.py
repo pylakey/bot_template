667e94b701c6cb8b1a28614d98e05cf134a97881
pylakey <pylakey@protonmail.com>
2022-01-08 18:24:00 +0300

    * improved decimal utils

--- {{ cookiecutter.project_name }}/app/utils/decimal.py
+++ {{ cookiecutter.project_name }}/app/utils/decimal.py
@@ -1,7 +1,18 @@
-from decimal import Decimal
+from decimal import (
+    Decimal,
+    ROUND_HALF_EVEN,
+)
 from typing import Union
 
 
-def normalize_decimal(d: Union[float, int, str, Decimal]) -> Decimal:
-    d = Decimal(d)
-    return d.quantize(Decimal(1)) if d == d.to_integral() else d.normalize()
+def normalize_decimal(
+        d: Union[float, int, str, Decimal],
+        auto_round: bool = True,
+        decimal_places: int = 8
+) -> Decimal:
+    result = Decimal(d)
+
+    if auto_round:
+        result = result.quantize(Decimal(10) ** (-decimal_places), rounding=ROUND_HALF_EVEN)
+
+    return result.quantize(Decimal(1)) if result == result.to_integral() else result.normalize()
