b3e2cc90dd5a946291616022db65d614ce6882fa
pylakey <pylakey@protonmail.com>
2022-01-16 20:04:21 +0300

    * Fixed command logging in log_middleware

--- {{ cookiecutter.project_name }}/app/bot/middlewares/log.py
+++ {{ cookiecutter.project_name }}/app/bot/middlewares/log.py
@@ -20,12 +20,15 @@ async def log_middleware(_, update: UpdateFromUser, call_next: CallNextMiddlewar
     elif isinstance(update, InlineQuery):
         logs.append(f"Query: {update.query or '-'}?offset={update.offset}")
     elif isinstance(update, Message):
-        if bool(update.command):
-            logs.append(f"Command: /{' '.join(update.command)}")
-        elif bool(update.media):
+        text = update.text or update.caption
+
+        if bool(update.media):
             logs.append(f"Media message")
-        else:
-            logs.append("Text message")
+        elif bool(text):
+            if text.startswith('/'):
+                logs.append(f"Command: {text}")
+            else:
+                logs.append("Text message")
     else:
         logger.info(f"[{update.__class__.__name__}] Unsupported update type")
         return await call_next(_, update)
